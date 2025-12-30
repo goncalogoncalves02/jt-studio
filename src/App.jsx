df5889e4b7f021d68bf48af2254d943dee7d56fa
Gonçalo Filipe Brigues Gonçalves <goncalo.goncalves.t0113779@edu.atec.pt>
2025-12-30 19:46:33 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen bg-slate-900">
+        <h1 className="text-3xl font-bold text-sky-400">
+          Olá, Tailwind em Portugal!
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
